0524791e3066136227af6fea1f63e145e2386b5c
Md Rahbir Mahdi <rahbir1518@gmail.com>
2025-03-21 10:28:59 -0400

    featured icon + div5 work

--- src/components/FeaturedProjects.jsx
+++ src/components/FeaturedProjects.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import SpotlightCard from './SpotlightCard'
+import project from '../assets/project.png';
 
 const FeaturedProjects = () => {
   return (
-    <SpotlightCard className="SpotlightCard5 div5 p-4 ml-7 bg-primary/60 hover:bg-primary transition-colors duration-450 rounded-2xl col-span-4 md:col-span-2 sm:col-span-1">5</SpotlightCard>
+    <SpotlightCard className="SpotlightCard5 div5 p-4 ml-7 bg-primary/60 hover:bg-primary transition-colors duration-450 rounded-2xl col-span-4 md:col-span-2 sm:col-span-1">
+      <div className='text-black font-primary'>
+        <h1 className="font-primary text-black text-2xl flex items-center space-x-2 hover:underline"> 
+          <img src={project} alt="Project icon by Icons8" className="w-6 h-6" />
+          <span>Featured Projects</span>
+        </h1>
+      </div>
+    </SpotlightCard>
   )
 }
 
