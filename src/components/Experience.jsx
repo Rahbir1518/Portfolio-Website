a61e8b0291dd0dda4ac2c1dafdf3b58b4565792b
Rahbir1518 <rahbir1518@gmail.com>
2026-01-09 18:50:18 -0500

    .

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,7 +15,6 @@ const Experience = () => {
           <h1 className='text-secondary font-primary text-lg pb-2'>
             <a
               className="text-black cursor-none hover:underline"
-              href=""
               target="_blank"
               rel="noopener noreferrer"
             >
