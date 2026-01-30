4ddb5c1aca0449e8818f0e27c5f6a6ffeae8e9f2
Rahbir1518 <rahbir1518@gmail.com>
2026-01-29 19:20:09 -0500

    final commit?

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,18 @@ export function Footer() {
         <a href="mailto:rahbir1518@gmail.com" className="footer-cta cursor-clickable">Get in Touch</a>
         
         <div className="footer-links">
-          <a href="https://www.linkedin.com/in/rahbirmahdi/" target="_blank" className="cursor-clickable
-">LinkedIn</a>
-          <a href="#" className="cursor-clickable">GitHub</a>
-          <a href="#" className="cursor-clickable">Devpost</a>
-          <a href="#" className="cursor-clickable">Resume</a>
+          <a href="https://www.linkedin.com/in/rahbirmahdi/" target="_blank" className="cursor-clickable">
+          LinkedIn
+          </a>
+          <a href="https://github.com/Rahbir1518" target="_blank" className="cursor-clickable">
+          GitHub
+          </a>
+          <a href="https://devpost.com/Rahbir1518?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav" target="_blank" className="cursor-clickable">
+          Devpost
+          </a>
+          <a href="/resume.pdf" download={"Md_Rahbir_Mahdi_Resume.pdf"} className="cursor-clickable">
+          Resume
+          </a>
         </div>
       </div>
     </footer>
